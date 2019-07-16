f560587fa426bba563b8d17dcb475fbd624b4ab5
Jordy Aaldering <aalderingjordy@gmail.com>
2019-07-16 22:46:26 +0200

    Removed Level Gen

--- Assets/Scripts/TargetFollow.cs
+++ Assets/Scripts/TargetFollow.cs
@@ -4,19 +4,20 @@ using UnityEngine;
 public class TargetFollow : MonoBehaviour
 {
 	[SerializeField] private Transform targetToFollow;
-	[SerializeField] private float dampTime = 0.15f;
 	
-    private Vector3 velocity = Vector3.zero;
+	[SerializeField] private float dampTime = 0.15f;
+	[SerializeField] private float xOffset = 5f;
+	[SerializeField] private float yOffset = 5f;
+
+	private Vector3 velocity = Vector3.zero;
 
 	public void FixedUpdate()
 	{
+		Vector3 pos = transform.position;
 		Vector3 destination = targetToFollow.position;
-		
-		Transform t = transform;
-		Vector3 pos = t.position;
 
-		destination.y = pos.y;
-		destination.z = pos.z;
+		destination.x += xOffset;
+		destination.y += yOffset;
         transform.position = Vector3.SmoothDamp(pos, destination, ref velocity, dampTime);
     }
 }
