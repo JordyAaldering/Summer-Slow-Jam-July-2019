dd5325150a09f1bb37276e976887446152f23539
Jordy Aaldering <aalderingjordy@gmail.com>
2019-07-19 14:10:58 +0200

    Walls

--- Assets/Scripts/Player/Bullet.cs
+++ Assets/Scripts/Player/Bullet.cs
@@ -9,4 +9,14 @@ public class Bullet : MonoBehaviour
         Rigidbody rb = GetComponent<Rigidbody>();
         rb.velocity = speed * transform.forward;
     }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        GameObject go = other.gameObject;
+        if (go.CompareTag("Wall"))
+        {
+            Destroy(go);
+            Destroy(gameObject);
+        }
+    }
 }
---
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerManager : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        GameObject go = other.gameObject;
+        if (go.CompareTag("Spike") || go.CompareTag("Wall"))
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
