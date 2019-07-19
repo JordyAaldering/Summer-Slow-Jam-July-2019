032296fce52ceb7ee657b5001103b77c8f4cc2de
Jordy Aaldering <aalderingjordy@gmail.com>
2019-07-19 15:20:59 +0200

    Basic Main Menu

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI
+{
+    public class MainMenu : MonoBehaviour
+    {
+        public void Start()
+        {
+            SceneManager.LoadScene(1);
+        }
+
+        public void Quit()
+        {
+            Application.Quit();
+        }
+    }
+}
