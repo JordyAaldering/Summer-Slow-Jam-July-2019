9717a06e495fad700c47247c6d5cb72ea6687d70
Jordy Aaldering <aalderingjordy@gmail.com>
2019-07-20 16:34:33 +0200

    Wall Prefab, Screenshots

---
+++ Assets/Scripts/ScreenShotter.cs
@@ -0,0 +1,20 @@
+﻿#if (UNITY_EDITOR)
+
+using System.IO;
+using UnityEngine;
+
+public class ScreenShotter : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F12))
+        {
+            DirectoryInfo di = new DirectoryInfo("Screenshots");
+            int i = di.GetFiles().Length + 1;
+            
+            ScreenCapture.CaptureScreenshot($"Screenshots/screen_{i}.png");
+        }
+    }
+}
+
+#endif
