732153aca942b904ac0f6138beee28770cae5941
Jordy Aaldering <aalderingjordy@gmail.com>
2019-07-20 16:44:22 +0200

    Made Screenshots

--- Assets/Scripts/ScreenShotter.cs
+++ Assets/Scripts/ScreenShotter.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 
 public class ScreenShotter : MonoBehaviour
 {
-    private void Update()
+    private void LateUpdate()
     {
         if (Input.GetKeyDown(KeyCode.F12))
         {
             DirectoryInfo di = new DirectoryInfo("Screenshots");
-            int i = di.GetFiles().Length + 1;
             
-            ScreenCapture.CaptureScreenshot($"Screenshots/screen_{i}.png");
+            int index = di.GetFiles().Length + 1;
+            string path = $"Screenshots/screen_{index}.png";
+            
+            ScreenCapture.CaptureScreenshot(path);
+            
+            Debug.Log($"Screenshot {index} saved to \"{path}\".");
         }
     }
 }
